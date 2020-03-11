0bffc11e9555b4bc55fbbb7dd7d81f7ac0468ff3
khhandrea <44332152+khhandrea@users.noreply.github.com>
2020-03-11 13:06:28 +0900

    각도와 초기 속도에 따른 발사 구현 완료

--- projectile motion.py
+++ projectile motion.py
@@ -14,6 +14,7 @@ from datetime import datetime
 from datetime import timedelta
 import random
 import pygame.font as font
+import math
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 420
@@ -24,26 +25,34 @@ OBJ_COLOR = 200, 0, 0
 FLOOR_COLOR = 204, 174, 98
 FLOOR_HEIGHT = 50
 
-GRAVITY = 0.00025
+GRAVITY = 0.4
+
+# initial statement
+v_0 = 12
+theta = 45
+
+rad = math.radians(theta)
+v_x0 = v_0*math.cos(rad)
+v_y0 = -v_0*math.sin(rad)
 
 
 class Object:
     color = OBJ_COLOR
     radius = 20
-    velocity = [0, 0]
+    velocity = [v_x0, v_y0]
 
     def __init__(self):
         self.x = self.radius
-        # self.y = SCREEN_HEIGHT-FLOOR_HEIGHT-self.radius
-        self.y = self.radius
+        self.y = SCREEN_HEIGHT-FLOOR_HEIGHT-self.radius-1
+        # self.y = self.radius
 
     def movement(self):
-        self.x += self.velocity[0]
         if self.y < SCREEN_HEIGHT-FLOOR_HEIGHT-self.radius:
             self.y += self.velocity[1]
+            self.x += self.velocity[0]
         else:
-            self.y = SCREEN_HEIGHT-FLOOR_HEIGHT-self.radius-1
-            self.velocity[1] *= -1
+            self.y = SCREEN_HEIGHT-FLOOR_HEIGHT-self.radius  # collision
+            # self.velocity[1] *= -1
 
     def draw(self):
         pg.draw.circle(screen, self.color,
@@ -87,6 +96,7 @@ pg.init()
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 game_board = GameBoard()
 FONT = pg.font.Font(None, 24)
+clock = pg.time.Clock()
 
 
 def draw_background(screen):
@@ -97,14 +107,18 @@ def draw_background(screen):
 while True:
     events = pg.event.get()
 
+    font = pg.font.Font(None, 30)
     for event in events:
         if event.type == pg.QUIT:  # quit
             exit()
 
-    game_board.object.velocity[0] = 0.02
+    game_board.object.velocity[0]
     game_board.object.velocity[1] += GRAVITY  # gravity
     game_board.object.movement()
 
     draw_background(screen)
     game_board.draw(screen)
-    pg.display.update()  # update
+    pg.display.flip()  # update
+    clock.tick(60)
+
+pg.quit()
